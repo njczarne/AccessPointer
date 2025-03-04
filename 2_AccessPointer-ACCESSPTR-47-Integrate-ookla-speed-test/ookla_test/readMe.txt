78a67893a50df0ea9ca838ff9e9f206c4dbc76f2
njczarne <njczarne@syr.edu>
2025-03-04 14:55:52 -0500

    Delete 2_AccessPointer-ACCESSPTR-47-Integrate-ookla-speed-test directory

--- 2_AccessPointer-ACCESSPTR-47-Integrate-ookla-speed-test/ookla_test/readMe.txt
+++
@@ -1,1 +0,0 @@
-This is just a test folder that will later be integrated into the main program
--- 2_AccessPointer-ACCESSPTR-47-Integrate-ookla-speed-test/tunnel.py
+++
@@ -1,5 +0,0 @@
-from pyngrok import ngrok
-
-def start_ngrok(port):
-    public_url = ngrok.connect(port, "http")
-    return public_url
